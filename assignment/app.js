5e52734c7cd1f108e923d5dc48d1926a576a21cd
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-09-27 00:26:30 +0500

    done almost

--- assignment/app.js
+++ assignment/app.js
@@ -1,7 +1,9 @@
+//ES6 Assignment # 1
+
 //========Nested Array Destructing=======
-let arr=[2,3,5,[33,55]]
-let [a,b,c]=arr
-let [,,,[a2,a3]]=arr
+let arr = [2, 3, 5, [33, 55]]
+let [a, b, c] = arr
+let [, , , [a2, a3]] = arr
 console.log(a)
 console.log(b)
 console.log(c)
@@ -10,17 +12,17 @@ console.log(a3)
 
 
 //========Nested Object Destructing========
-let obj={
-    name:{
-        fname:"wajahat",
-        lname:"ahmed"
+let obj = {
+    name: {
+        fname: "wajahat",
+        lname: "ahmed"
     },
-    age:23,
-    institute:"Adamjee"
+    age: 23,
+    institute: "Adamjee"
 }
 
-let {name,age,institute}=obj
-let {fname,lname}=name
+let { name, age, institute } = obj
+let { fname, lname } = name
 console.log(name)
 console.log(fname)
 console.log(lname)
@@ -30,94 +32,172 @@ console.log(institute)
 
 //check properties and values from a object
 
-let obj1={
-    name:"wajahat",
-    age:19,
-    institute:"NED University"
+let obj1 = {
+    name: "wajahat",
+    age: 19,
+    institute: "NED University"
 }
 
-let objKeys=Object.keys(obj1)
-let objValues=Object.values(obj1)
+let objKeys = Object.keys(obj1)
+let objValues = Object.values(obj1)
 
 console.log(objKeys)
 console.log(objValues)
 
 let std = [
     {
-        name: 'Ali',
+        name: 'ali',
         class: '1',
-        age:20,
-        cnic:"42101-80566579"
+        age: 7,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Usama',
+        name: 'wajahat',
         class: '1',
-        age:20,
-        cnic:"42101-80566579"
+        age: 7,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Ali',
+        name: 'ali',
         class: '6',
-        age:20,
-        cnic:"42101-80566579"
+        age: 12,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Waqas',
+        name: 'waqas',
         class: '4',
-        age:20,
-        cnic:"42101-80566579"
+        age: 10,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Ali',
+        name: 'ali',
         class: '1',
-        age:20,
-        cnic:"42101-80566579"
+        age: 7,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Usama',
+        name: 'wajahat',
         class: '2',
-        age:20,
-        cnic:"42101-80566579"
+        age: 6,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Waqas',
+        name: 'waqas',
         class: '1',
-        age:20,
-        cnic:"42101-80566579"
+        age: 6,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Usama',
+        name: 'wajahat',
         class: '1',
-        age:20,
-        cnic:"42101-80566579"
+        age: 6,
+        cnic: "42101-80566579"
     },
     {
-        name: 'Ali',
+        name: 'ali',
         class: '4',
-        age:20,
-        cnic:"42101-80566579"
+        age: 10,
+        cnic: "42101-80566579"
     },
-    
+
     {
-        name: 'Ali',
-        class: '4',
-        age:20,
-        cnic:"42101-80566579"
+        name: 'ali',
+        class: '5',
+        age: 10,
+        cnic: "42101-80566579"
     }
 ]
 
-function searched(){
-    let val=document.getElementById("searched")
-    switch(val.selectedIndex){
-        case 1 :
-            console.log("Usama Found")
-        case 2:
-            console.log("Ali Found")
-        case 3:
-            console.log("Basit Found")
-        case 4:
-            console.log("Wajahat Found")
+function searched() {
+    let searchQuery = document.getElementById("search")
+    let val = document.getElementById("searched")
+    document.getElementById('ans').innerHTML = ""
+    // switch(val.selectedIndex){
+    //     case 1 :
+    //         console.log("Class 1")
+    //         break;
+    //     case 2:
+    //         console.log("Class 2")
+    //         break;
+    //     case 3:
+    //         console.log("Class 3")
+    //         break;
+    //     case 4:
+    //         console.log("Class 4")
+    //         break;
+    // }
+    document.getElementById('ans1').innerHTML = `<h1>Bio Data of Selected Students <h1>`
+    let count = 0
+    for (var i = 0; i < std.length; i++) {
+        if (std[i].name == searchQuery.value.toLowerCase() && val.selectedIndex === 0) {
+            count++
+            document.getElementById('ans').innerHTML += `<table class="center">
+            <tr>
+            <th >Name</th>
+            <th >Class</th>
+            <th >Age</th>
+            <th colspan=2>CNIC</th>
+        </tr>
+            <tbody>
+            <tr>
+            <td >${std[i].name}</td>
+            <td >${std[i].class}</td>
+            <td >${std[i].age}</td>
+            <td >${std[i].cnic}</td>
+        </tr>
+    </tbody>
+    </table>`
+            // document.getElementById('ans').innerHTML+=``
 
+            console.log(std[i])
+
+        }
+        if (std[i].class == val.selectedIndex && searchQuery.value.length == 0) {
+            count++;
+            document.getElementById('ans').innerHTML += `<table class="center">
+            <tr>
+            <th >Name</th>
+            <th >Class</th>
+            <th >Age</th>
+            <th colspan=2>CNIC</th>
+        </tr>
+            <tbody>
+            <tr>
+            <td >${std[i].name}</td>
+            <td >${std[i].class}</td>
+            <td >${std[i].age}</td>
+            <td >${std[i].cnic}</td>
+        </tr>
+    </tbody>
+    </table>`
+            // document.getElementById('ans').innerHTML+=``
+
+            console.log(std[i])
+
+        }
+        if (std[i].name == searchQuery.value.toLowerCase() && std[i].class == val.selectedIndex) {
+            count++;
+            document.getElementById('ans').innerHTML += `<table class="center">
+            <tr>
+            <th >Name</th>
+            <th >Class</th>
+            <th >Age</th>
+            <th colspan=2>CNIC</th>
+        </tr>
+            <tbody>
+            <tr>
+            <td >${std[i].name}</td>
+            <td >${std[i].class}</td>
+            <td >${std[i].age}</td>
+            <td >${std[i].cnic}</td>
+        </tr>
+    </tbody>
+    </table>`
+            // document.getElementById('ans').innerHTML+=``
+            console.log(std[i])
+        }
+        else if (count === 0 && i + 1 === std.length) {
+            document.getElementById('ans').innerHTML = `<h2> No Data Found </h2>`
+        }
     }
-   
 }
