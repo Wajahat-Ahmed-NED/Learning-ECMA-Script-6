b5a7f3c0a0fab23bef93199230a0f73901f070f3
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-10-07 12:51:50 +0500

    done

--- class5/app.js
+++ class5/app.js
@@ -7,18 +7,41 @@
 //     }
 // }
 
-
 // function baar() {
 //     let a = 'Return Value'
 //     return a
 // }
 
-
-
 // foo('abc')()
 
 
+//=== my practice ====
+// let a;
+// function  foo(val){
+//     a=val?val:"write something"
+//     console.log('first write')
+//     return function baar(){
+//         console.log(a)
+//     }
+// }
+// foo('abc')()
 
+// console.log('ye ma ho')
+
+
+// function  foo(val){
+//     a=val?val:"write something"
+
+//     console.log(a) 
+// }
+
+// function baar(){
+//     let a="return me"
+//     return a
+// }
+
+// foo(baar())
+//=====my code ends here ====
 
 
 // const b = []
@@ -30,15 +53,36 @@
 //     console.log('hello worlfd')
 // }
 
+// let fun=function(){
+//     console.log('hello world')
+// }
+// fun()
+
 // let baar = (val) => {
 //     return val
 // }
 
-// let baar = val => {
+// let baar= (val)=>{
 //     return val
 // }
+// console.log(baar('gullo ')) 
+
+
+// let baar = val => {
+    //     return val
+    // }
+    
+    // let baar=val=>val;
+    // console.log(baar('pyaro')) 
+ 
 // let c = 'asdasd'
 // let baar = _ => console.log(_)
+
+
+// let c='kdhf'
+// let baar=_=>console.log(_)
+// baar(c)
+
 
 // let arr = [ 6, 7, 8, 9]
 // let fil = arr.filter(x => x < 5)
@@ -46,6 +90,10 @@
 
 // console.log(fil)
 
+// let arr=[3,1,4,6,67,3,6,2]
+// let fill=arr.filter(x=>x<5)
+// fill=fill.length<0?"no value found":fill;
+// console.log(fill)
 
 
 // baar('without curly Bracket')
@@ -60,6 +108,16 @@
 // console.log(3)
 // console.log(4)
 
+// console.log(1)
+// setTimeout(() => {
+//     console.log(2)
+    
+// }, 3000);
+
+
+// console.log(3)
+// console.log(4)
+
 
 //     let db = new Promise((resolve,reject)=>{
 //         if(user == true){
@@ -71,8 +129,20 @@
 
 // db.then().catch()
 
+// let db=new Promise((resolve,reject)=>{
+//     if(user==true){
+//         resolve(data)
+//     }
+//     else{
+//         reject(data)
+//     }
+// })
+// db.then().catch()
+
 // async function foo(){
 //     let a = await data
 // }
 
-
+// async function foo(){
+//     let a=await data
+// }
