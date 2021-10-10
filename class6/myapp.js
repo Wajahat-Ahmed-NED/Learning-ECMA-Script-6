e60fe7a891a3608c3d8a33b2a4a4716748e4fa74
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-10-10 13:11:16 +0500

    practiced class 6

--- class6/myapp.js
+++ class6/myapp.js
@@ -1,11 +1,46 @@
 // This files contains practice material by me
 
-class Generate{
-    constructor(name,age){
-        this.userName=name
-        this.userAge=age
+// class Generate{
+//     constructor(name,age){
+//         this.userName=name
+//         this.userAge=age
+//     }
+// }
+// var g1=new Generate('wajahat',19)
+// console.log(g1)
+
+// console.log(this)
+// var bar='some text'
+// console.log(this.bar)
+// console.log(this.g1)
+
+// function foo(){
+//     console.log(this)
+// }
+// foo()
+
+
+// let a=[1,2,3,5,67,6,5]
+// let b='abcdefghijklmnop'
+// for(i of a){
+//     console.log(i)
+// }
+// for(i of b){
+//     console.log(i)
+// }
+
+let date;
+let foo=(username="no user")=>{
+    console.log(username)
+    if(username){
+        console.log('work done')
+        date=new Date()
+        console.log(date)
     }
 }
-var g1=new Generate('wajahat',19)
-console.log(g1)
+foo('wajahat')
 
+let api='https://jsonplaceholder.typicode.com/photos'
+fetch(api)
+.then(response=>response.json())
+.then(data=>console.log(data))
