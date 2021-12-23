cc28f65b5d07066c0decdac612af886858d9463d
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-12-24 00:34:55 +0500

    doing

--- closures/app.js
+++ closures/app.js
@@ -1,7 +1,11 @@
+// Closures ==> an inner function has access to the outer (enclosing) function's variables
+
+//----------Example 1-----------
 var sum=function(a){
     console.log("Hello Wajahat "+a)
     var c=4;
     return function(b){
+        // ayesa scope jo is function ko accessible ha wohi ha lexical scope jese c ka or a ka scope lexical ha
         return a+b+c
     }
 
@@ -16,3 +20,22 @@ console.log(store(4))
 // yaha hamne store ke andr jo function ha usko call kya to wo 
 // parent ki value ko access krpa rha ha 
 // parent ki value lost nahi horahi
+
+//-----------Example 2 ---------------
+
+var mult=function(a,b,c){
+
+
+    return {
+        getTwo:function(){
+            return a+b
+        },
+        getThree:function(){
+            return a+b+c
+        }
+    }
+}
+
+var store1=mult(1,23,4)
+console.log(store1.getTwo())
+console.log(store1.getThree())
